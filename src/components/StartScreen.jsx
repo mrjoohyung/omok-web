aaa983d9f1f62a0ffe43c3db6ac6aee2e01b8321
mrjoohyung <mrjoohyung@gmail.com>
2026-05-03 22:45:29 +0900

    Update StartScreen.jsx

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -15,7 +15,8 @@ export default function StartScreen({
   const [hintEnabled, setHintEnabled] = useState(true);
   const [showThreatsPvp, setShowThreatsPvp] = useState(false);
   const [pvpRecordMode, setPvpRecordMode] = useState('guest');
-  const [blackLabel, setBlackLabel] = useState('anonymous');
+  const [blackLabel, setBlackLabel] = useState('self');
+  const [blackLabel, setBlackLabel] = useState('self');
   const [whiteLabel, setWhiteLabel] = useState('anonymous');
 
   const [aiLevel, setAiLevel] = useState(3);
